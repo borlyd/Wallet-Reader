8f8e4ff78db853af517ce733a7b5204c5ad95425
Lydia Borkova <lydiaborkova@gmail.com>
2022-03-21 12:42:03 +0100

    buffered reader

--- .idea/src/main/java/sk.borlyd.core/App.java
+++ .idea/src/main/java/sk.borlyd.core/App.java
@@ -4,16 +4,25 @@ package sk.borlyd.core;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class App {
     public static void main(String args[]) throws Exception {
-    String path = "";
+    String path = "penazenka";
+    String line = "";
 
     try {
         BufferedReader br = new BufferedReader(new FileReader(path));
+        while ((line = br.readLine()) != null) {
+            System.out.println(line);
+
+        }
     }
     catch (FileNotFoundException e) {
         e.printStackTrace();
         }
+    catch (IOException e) {
+        e.printStackTrace();
+    }
     }
 }
